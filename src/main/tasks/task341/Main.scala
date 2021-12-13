ce1b64e9d5da77964c16aa0e9b76121af0e85996
ryskit <ryosukekida.71@gmail.com>
2021-12-13 17:56:25 +0900

    fix task341

--- src/main/tasks/task341/Main.scala
+++ src/main/tasks/task341/Main.scala
@@ -6,24 +6,20 @@ object Main extends App {
   val n, m = sc.nextInt
   val as = Vector.fill(n - 1)(sc.nextInt)
   val bs = Vector.fill(n - 1)(sc.nextInt)
-  val dp: Array[Array[Boolean]] = Array.ofDim(n, m)
-  val score: Array[Array[Long]] = Array.ofDim(n, m)
+  val dp: Array[Array[Long]] = Array.fill(n)(Array.fill(m)(-1L))
 
-  dp(0)(0) = true
-  score(0)(0) = 0
+  dp(0)(0) = 0
 
   (0 until n - 1).foreach { i =>
     (0 until m).foreach { j =>
-      if (dp(i)(j)) {
-        dp(i + 1)(j) = true
-        score(i + 1)(j) = score(i + 1)(j).max(score(i)(j) + 0)
+      if (dp(i)(j) != -1) {
+        dp(i + 1)(j) = dp(i + 1)(j).max(dp(i)(j) + 0)
         val right = as(i) + j
         if (right <= m - 1) {
-          dp(i + 1)(right) = true
-          score(i + 1)(right) = score(i + 1)(right).max(score(i)(j) + bs(i))
+          dp(i + 1)(right) = dp(i + 1)(right).max(dp(i)(j) + bs(i))
         }
       }
     }
   }
-  println(if (dp(n - 1)(m - 1)) score(n - 1)(m - 1) else - 1)
+  println(dp(n - 1)(m - 1))
 }
